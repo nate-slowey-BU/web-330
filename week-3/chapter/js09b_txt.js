c349c945c1e735ba6ae382d43f8df22b810e26a9
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-11-05 20:49:08 -0600

    week 3 chapter complete

---
+++ week-3/chapter/js09b_txt.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+
+      Eating Well in Season
+      Author: Nate Slowey
+      Date: 11/4
+
+      Filename: js09b.js
+ */
